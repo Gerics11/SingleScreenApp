85b13c3ab3d9c46ea9ca1e6d5be0ccc3694255b1
Gerics11 <feher.gergo9@gmail.com>
2017-03-20 18:56:15 +0100

    initial commit

---
+++ OneLifeOneLove/app/src/main/java/com/example/android/onelifeonelove/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.android.onelifeonelove;
+
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView title1 = (TextView)findViewById(R.id.title1);
+        Typeface customFont = Typeface.createFromAsset(getAssets(), "fonts/glossandbloom.ttf");
+        title1.setTypeface(customFont);
+    }
+}
